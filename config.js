9e1e86be5ad5f0275660f0b1cc9af18e79c95fb2
tebmail01 <tebmail01@gmail.com>
2026-04-19 13:03:11 +0200

    Configure hosted Google sync settings

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.OPERATION_LOG_CONFIG = window.OPERATION_LOG_CONFIG || {
-  googleClientId: "",
-  googleApiKey: "",
+  googleClientId: "459904705124-lebfvuk965mvt0vctmoe7t781n8nbg33.apps.googleusercontent.com",
+  googleApiKey: "AIzaSyDy9AuU6WSi_2QNA1hk2v-iL8mIaXhqEgc",
+  googleApiKeyBackup: "AIzaSyANOrIG6xt56R9A7p9dTL9Q2BRXv8ohGXw",
   googleAppName: "Operation Log",
   driveFolderName: "Operation Log Backups",
   driveFileName: "operation-log-sync.json",
