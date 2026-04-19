27279655672044fa5a1d136f2589c284eff61cdc
tebmail01 <tebmail01@gmail.com>
2026-04-19 12:05:46 +0200

    Configure hosted app spreadsheet target

--- config.js
+++ config.js
@@ -4,4 +4,6 @@ window.OPERATION_LOG_CONFIG = window.OPERATION_LOG_CONFIG || {
   googleAppName: "Operation Log",
   driveFolderName: "Operation Log Backups",
   driveFileName: "operation-log-sync.json",
+  googleSpreadsheetId: "1LJ9JRCPOsIFkEocR4P86lSto_xtECmhGKhmLpPpvTMI",
+  googleSheetName: "Cases",
 };
