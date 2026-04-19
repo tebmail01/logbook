31a8c1ecb43d088dc350e08c030e61c182ce7545
tebmail01 <tebmail01@gmail.com>
2026-04-19 12:05:49 +0200

    Add config example for hosted app setup

---
+++ config.example.js
@@ -0,0 +1,9 @@
+window.OPERATION_LOG_CONFIG = {
+  googleClientId: "YOUR_GOOGLE_OAUTH_CLIENT_ID",
+  googleApiKey: "YOUR_GOOGLE_API_KEY",
+  googleAppName: "Operation Log",
+  driveFolderName: "Operation Log Backups",
+  driveFileName: "operation-log-sync.json",
+  googleSpreadsheetId: "YOUR_GOOGLE_SPREADSHEET_ID",
+  googleSheetName: "Cases",
+};
