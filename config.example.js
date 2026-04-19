fa9fb92256584145c537d8ad10a60de3045bbd9f
tebmail01 <tebmail01@gmail.com>
2026-04-19 13:03:50 +0200

    Document backup API key option

--- config.example.js
+++ config.example.js
@@ -1,6 +1,7 @@
 window.OPERATION_LOG_CONFIG = {
   googleClientId: "YOUR_GOOGLE_OAUTH_CLIENT_ID",
   googleApiKey: "YOUR_GOOGLE_API_KEY",
+  googleApiKeyBackup: "OPTIONAL_BACKUP_GOOGLE_API_KEY",
   googleAppName: "Operation Log",
   driveFolderName: "Operation Log Backups",
   driveFileName: "operation-log-sync.json",
